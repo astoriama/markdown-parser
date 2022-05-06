8634413eca43bda64bb4b5e5f9faf3882a44c446
astoriama <yim016@ucsd.edu>
2022-05-06 14:27:29 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -26,12 +26,8 @@ public class MarkdownParse {
                     if(markdown.indexOf(")", closeBracketOpenParen)==-1){
                         break;
                     }else{
-                        int nextOpenBracket = markdown.indexOf("[", closeBracketOpenParen);
                         int closeParen = markdown.indexOf(")", closeBracketOpenParen);
-                        while(closeParen < nextOpenBracket){
-                            closeParen = markdown.indexOf(")", closeParen+1);
-                        }                      
-                        toReturn.add(markdown.substring(closeBracketOpenParen +2, closeParen));
+                        toReturn.add(markdown.substring(closeBracketOpenParen + 2, closeParen));
                         currentIndex = closeParen + 1;
                     }
                 }
